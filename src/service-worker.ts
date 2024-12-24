88cfe990593eaa78c3846b467271406593982a67
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-12-24 18:51:26 +0530

    feat: add install and activate service worker
    - this service worker will add files to cache also remove old cache on active event.

---
+++ src/service-worker.ts
@@ -0,0 +1,41 @@
+/// <reference types="@sveltejs/kit" />
+/// <reference lib="webworker" />
+
+declare let self: ServiceWorkerGlobalScope
+
+import { build, files, version } from '$service-worker';
+
+// Create a unique cache name for this deployment
+const CACHE = `cache-${version}`;
+
+const ASSETS = [
+	...build, // the app itself
+	...files,  // everything in `static`
+];
+
+// console.log({ build, files, version });
+
+
+// install service worker
+self.addEventListener('install', (event) => {
+	// Create a new cache and add all files to it
+	async function addFilesToCache() {
+		const cache = await caches.open(CACHE);
+		await cache.addAll(ASSETS);
+	}
+
+	event.waitUntil(addFilesToCache());
+});
+
+
+// activate service worker
+self.addEventListener('activate', (event) => {
+	// Remove previous cached data from disk
+	async function deleteOldCaches() {
+		for (const key of await caches.keys()) {
+			if (key !== CACHE) await caches.delete(key);
+		}
+	}
+
+	event.waitUntil(deleteOldCaches());
+});
