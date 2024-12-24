ce879a65fdafc7d0101be8f1208ee28228d12276
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-12-24 16:45:24 +0530

    style: add classes to about page elements

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,9 +1,16 @@
-<h1>About <code>TODO</code></h1>
+<div class = "box">
+    <h1>About <code>TODO</code></h1>
 
-<p>
-    <code>TODO:</code> Your ultimate productivity companion!
-    <br />
-    We understand how challenging it can be to juggle multiple tasks, meet deadlines, and stay organized in today’s fast-paced world.
-    <br />
-    That’s why we created <code>TODO</code> — a simple, intuitive, and efficient to-do list platform designed to help you stay on top of your goals and make every day productive.
-</p>
+    <p>
+        <code>TODO:</code> Your ultimate productivity companion!
+    </p>
+    <p>
+        We understand how challenging it can be to juggle multiple tasks, meet deadlines,
+        and stay organized in today’s fast-paced world.
+    </p>
+    <p>
+        That’s why we created <code>TODO</code> — a simple, intuitive, and efficient
+        to-do list platform designed to help you stay on top of your goals and make
+        every day productive.
+    </p>
+</div>
